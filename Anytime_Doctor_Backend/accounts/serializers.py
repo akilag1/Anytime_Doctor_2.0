b92543285dabc1d6857b685b174cb0705459e3c4
KingJonAG <akilagodigamuwa@gmail.com>
2021-03-04 22:59:58 +0530

    update6

--- Anytime_Doctor_Backend/accounts/serializers.py
+++ Anytime_Doctor_Backend/accounts/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
 from django.contrib.auth.models import User
-# from .models import UserExtra
+from .models import UserExtra
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
+    
+class UserExSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserExtra
+        fields = ('user','contact_no')
+        lookup_field = 'user'
